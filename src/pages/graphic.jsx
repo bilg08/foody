4000ba8e032fe899dd5236cad47a321663f9f802
bilguunZorigooLeap1 <bilguunzorigooleap1@iMac2021-02s-iMac.local>
2022-09-26 10:47:58 +0800

    negActions3222

--- src/pages/graphic.jsx
+++ src/pages/graphic.jsx
@@ -46,8 +46,8 @@ export const Graphic = () => {
         label: "Захиалга өдөр өдрөөр",
         data: allOrdersDetails.ordersDataArray,
         backgroundColor: [
-          "rgba(255, 159, 64, 0.2)",
           "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
           "rgba(255, 206, 86, 0.2)",
           "rgba(75, 192, 192, 0.2)",
           "rgba(153, 102, 255, 0.2)",
