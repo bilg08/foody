f1eaf1817116a0700c7bcf84ac2ed8eada96b0bc
bilguunZorigooLeap1 <bilguunzorigooleap1@iMac2021-02s-iMac.local>
2022-09-26 10:27:29 +0800

    actions2

--- src/pages/loginPage.jsx
+++ src/pages/loginPage.jsx
@@ -67,8 +67,8 @@ export default function SignInSide() {
           sm={4}
           md={7}
           sx={{
-            backgroundImage:
-              "url(https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1173&q=80)",
+            // backgroundImage:
+              // "url(https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1173&q=80)",
             backgroundRepeat: "no-repeat",
             backgroundColor: (t) =>
               t.palette.mode === "light"
